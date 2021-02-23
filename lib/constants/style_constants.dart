1adac0730787b36818dee037da1e52d77e89c04c
Pablofr10 <rafanet.live@gmail.com>
2021-02-22 23:17:26 -0300

    Adicionando texto principal.

---
+++ lib/constants/style_constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:travelApp/constants/color_constants.dart';
+
+var mTitleStyle = GoogleFonts.inter(
+    fontWeight: FontWeight.w600, color: mTitleColor, fontSize: 16);
+
+var mMoreDiscountStyle = GoogleFonts.inter(
+    fontSize: 12, fontWeight: FontWeight.w700, color: mBlueColor);
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:travelApp/constants/color_constants.dart';
+import 'package:travelApp/constants/style_constants.dart';
 import 'package:travelApp/widgets/bottom_navigation_travelkuy.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -19,7 +20,23 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       backgroundColor: mBackgroundColor,
       bottomNavigationBar: BottomNavigationTravelkuy(),
-      body: Container(),
+      body: Container(
+        child: ListView(
+          physics: ClampingScrollPhysics(),
+          children: [
+            Padding(
+              padding: EdgeInsets.only(
+                left: 16,
+                right: 16,
+              ),
+              child: Text(
+                'Hi, Pablo. This Promos for you!',
+                style: mTitleStyle,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
