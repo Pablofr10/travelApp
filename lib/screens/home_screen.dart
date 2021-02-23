2350f3a66b3d991aded289170232d8de9820cb57
Pablofr10 <rafanet.live@gmail.com>
2021-02-23 20:04:19 -0300

    Aldicionando slider e textos.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:travelApp/constants/color_constants.dart';
 import 'package:travelApp/constants/style_constants.dart';
+import 'package:travelApp/models/carousel_model.dart';
 import 'package:travelApp/widgets/bottom_navigation_travelkuy.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -10,6 +12,16 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _current = 0;
+
+  List<T> map<T>(List list, Function handler) {
+    List<T> result = [];
+    for (var i = 0; i < list.length; i++) {
+      result.add(handler(i, list[i]));
+    }
+    return result;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,11 +39,130 @@ class _HomeScreenState extends State<HomeScreen> {
             Padding(
               padding: EdgeInsets.only(
                 left: 16,
-                right: 16,
+                bottom: 24,
               ),
               child: Text(
                 'Hi, Pablo. This Promos for you!',
                 style: mTitleStyle,
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              margin: EdgeInsets.only(left: 16, right: 16),
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 190,
+                    child: Swiper(
+                      onIndexChanged: (index) {
+                        setState(() {
+                          _current = index;
+                        });
+                      },
+                      autoplay: true,
+                      layout: SwiperLayout.DEFAULT,
+                      itemCount: carousels.length,
+                      itemBuilder: (BuildContext context, index) {
+                        return Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            image: DecorationImage(
+                              image: AssetImage(
+                                carousels[index].image,
+                              ),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 12),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: map<Widget>(carousels, (index, image) {
+                          return Container(
+                            alignment: Alignment.centerLeft,
+                            height: 6,
+                            width: 6,
+                            margin: EdgeInsets.only(right: 8),
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: _current == index
+                                    ? mBlueColor
+                                    : mGreyColor),
+                          );
+                        }),
+                      ),
+                      Text(
+                        'More...',
+                        style: mMoreDiscountStyle,
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                left: 16,
+                top: 24,
+              ),
+              child: Text(
+                'Let\'s Booking',
+                style: mTitleStyle,
+              ),
+            ),
+            Container(
+              height: 144,
+              margin: EdgeInsets.only(left: 16, right: 16),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(right: 8),
+                        padding: EdgeInsets.only(left: 16),
+                        height: 64,
+                        decoration: BoxDecoration(
+                          color: mFillColor,
+                          borderRadius: BorderRadius.circular(12),
+                          border: Border.all(color: mBorderColor, width: 1),
+                        ),
+                        child: Row(
+                          children: [
+                            SvgPicture.asset(
+                              'assets/svg/service_flight_icon.svg',
+                              fit: BoxFit.contain,
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(left: 16),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    'Flight',
+                                    style: mServiceTitleStyle,
+                                  ),
+                                  Text(
+                                    'Feel freedom',
+                                    style: mServiceSubTitleStyle,
+                                  )
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  )
+                ],
               ),
             ),
           ],
